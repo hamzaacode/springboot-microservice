84c8ed26ba79cedf0b177db22ddc8d4194218521
md.amirhamza <amir.hamza.code@gmail.com>
2025-03-08 19:58:00 +0600

    Added new module user service

---
+++ user-service/src/main/java/org/example/userservice/UserController.java
@@ -0,0 +1,13 @@
+package org.example.userservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return "List of Users";
+    }
+}
