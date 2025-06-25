242dfcec531669689706d2371dd9d95b39f9af1d
md.amirhamza <amir.hamza.code@gmail.com>
2025-06-25 21:31:12 +0600

    Added API gateway Added service registry with eureka.

--- user-service/src/main/java/org/example/userservice/UserController.java
+++ user-service/src/main/java/org/example/userservice/UserController.java
@@ -3,11 +3,13 @@ package org.example.userservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class UserController {
 
     @GetMapping("/users")
     public String getUsers() {
+        System.out.println("getUsers");
         return "List of Users";
     }
 }
