8a0c294c136b5d77acc32b7289c07c062802c8f0
md.amirhamza <amir.hamza.code@gmail.com>
2025-07-14 22:11:29 +0600

    Added kubernates deployment & service for user-service, order-service , eureka-server .

--- order-service/src/main/java/org/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/org/example/orderservice/controller/OrderController.java
@@ -1,15 +1,17 @@
 package org.example.orderservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 public class OrderController {
 
     @GetMapping("/orders")
     public String getOrders() {
         System.out.println("getOrders");
+        log.info("Get order list successful");
         return "Order Service: List of orders";
     }
 }
--- user-service/src/main/java/org/example/userservice/UserController.java
+++ user-service/src/main/java/org/example/userservice/UserController.java
@@ -1,15 +1,16 @@
 package org.example.userservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 public class UserController {
 
     @GetMapping("/users")
     public String getUsers() {
-        System.out.println("getUsers");
+        log.info("Get user list successfully");
         return "List of Users";
     }
 }
