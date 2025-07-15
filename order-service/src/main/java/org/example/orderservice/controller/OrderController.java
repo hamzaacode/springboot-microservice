40956fc3d9b66d4d3e385c3110ad2114f2c75ab3
md.amirhamza <amir.hamza.code@gmail.com>
2025-07-15 23:37:48 +0600

    Added a order response and updated request mapping

--- order-service/src/main/java/org/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/org/example/orderservice/controller/OrderController.java
@@ -1,17 +1,32 @@
 package org.example.orderservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.orderservice.dto.OrderResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
 public class OrderController {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
     @GetMapping("/orders")
-    public String getOrders() {
-        System.out.println("getOrders");
+    public OrderResponse getOrders() {
         log.info("Get order list successful");
-        return "Order Service: List of orders";
+
+        // Example dummy data
+        List<String> orders = List.of("Order 1", "Order 2", "Order 3");
+
+        return new OrderResponse(
+                "Fetched order list successfully",
+                "SUCCESS",
+                orders
+        );
     }
 }
