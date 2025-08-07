51a1cf69ce6e2ee052cc1e2595e47c9c3d53d99e
md.amirhamza <amir.hamza.code@gmail.com>
2025-08-07 23:54:47 +0600

    test commit

--- order-service/src/main/java/org/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/org/example/orderservice/controller/OrderController.java
@@ -17,6 +17,7 @@ public class OrderController {
 
     @GetMapping("/orders")
     public OrderResponse getOrders() {
+
         log.info("Get order list successfully");
         List<String> orders = List.of("Order 1", "Order 2", "Order 3");
         return new OrderResponse(
